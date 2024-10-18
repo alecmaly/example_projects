97713936eb16dbe3f9ce7b31f46f472ef1cd4934
alec <alecjmaly@gmail.com>
2024-10-18 13:49:51 -0400

    updated

---
+++ rust/target/debug/deps/pin_project_lite-2f114f0ea34e5e0c.d
@@ -0,0 +1,5 @@
+/home/alec/Desktop/example_projects/rust/target/debug/deps/libpin_project_lite-2f114f0ea34e5e0c.rmeta: /home/myuser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs
+
+/home/alec/Desktop/example_projects/rust/target/debug/deps/pin_project_lite-2f114f0ea34e5e0c.d: /home/myuser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs
+
+/home/myuser/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs:
---
+++ rust/target/debug/deps/rust_example-23f5a2d7fff5e93f.d
@@ -0,0 +1,7 @@
+/home/alec/Desktop/example_projects/rust/target/debug/deps/librust_example-23f5a2d7fff5e93f.rmeta: src/main.rs src/module1.rs src/module2.rs
+
+/home/alec/Desktop/example_projects/rust/target/debug/deps/rust_example-23f5a2d7fff5e93f.d: src/main.rs src/module1.rs src/module2.rs
+
+src/main.rs:
+src/module1.rs:
+src/module2.rs:

af15bf73c5b1acceb97b9064514a25ecb233bcf4
alec <alecjmaly@gmail.com>
2025-09-28 15:36:23 -0400

    updated scan results for each project

--- rust/target/debug/deps/pin_project_lite-9e40b2b21beb4245.d
+++
@@ -1,5 +0,0 @@
-/home/alec/Desktop/example_projects/rust/target/debug/deps/libpin_project_lite-9e40b2b21beb4245.rmeta: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs
-
-/home/alec/Desktop/example_projects/rust/target/debug/deps/pin_project_lite-9e40b2b21beb4245.d: /root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs
-
-/root/.cargo/registry/src/index.crates.io-6f17d22bba15001f/pin-project-lite-0.2.14/src/lib.rs:
--- scan_all.sh
+++ scan_all.sh
@@ -2,7 +2,6 @@
 find . -name "*.json" -type f ! -name launch.json ! -name tsconfig.json ! -path "*/_solidity-chainlink/*" ! -path "*/_vulnserver/*"  -delete
 
 language_folders=(asm bash csharp c go java kotlin lua php powershell python ruby rust solidity typescript)
-language_folders=(csharp)
 BASE_DIR="`pwd`/"
 
 for language in "${language_folders[@]}"
@@ -17,36 +16,37 @@ do
     fi
 
     # step 1: parse codebase
-    docker run --rm -it -p 5678:5678 -v "$(pwd)":/app/output -v "$src_dir":$src_dir alecmaly/sa-tool python3 /app/1_extract_w_lsp.py -d $src_dir -l $language 
-    
+    docker run --rm -it -v "$(pwd)":/app/output -v "$src_dir":$src_dir alecmaly/sa-tool python3 /app/1_extract_w_lsp.py -d $src_dir -l $language    
 
     docker run --rm -it -v $(pwd):/app/output alecmaly/sa-tool python3 /app/2_build_callstacks.py
 
     # Step 3: move files to .vscode for extension
     mkdir -p .vscode/ext-static-analysis/graphs
-    mv ./functions_html.json ./.vscode/ext-static-analysis/functions_html.json
-    mv ./decorations.json ./.vscode/ext-static-analysis/decorations.json
-    mv ./callstacks.json ./.vscode/ext-static-analysis/callstacks.json
-    mv ./scope_summaries_html.json ./.vscode/ext-static-analysis/scope_summaries_html.json
-    mv ./inheritance_graph.json ./.vscode/ext-static-analysis/graphs/inheritance_graph.json
+    mv ./.vscode/ext-static-analysis/cache/functions_html.json ./.vscode/ext-static-analysis/functions_html.json
+    mv ./.vscode/ext-static-analysis/cache/decorations.json ./.vscode/ext-static-analysis/decorations.json
+    mv ./.vscode/ext-static-analysis/cache/callstacks.json ./.vscode/ext-static-analysis/callstacks.json
+    mv ./.vscode/ext-static-analysis/cache/scope_summaries_html.json ./.vscode/ext-static-analysis/scope_summaries_html.json
+    mv ./.vscode/ext-static-analysis/cache/inheritance_graph.json ./.vscode/ext-static-analysis/graphs/inheritance_graph.json
 
 
     src_dir=`pwd` && docker run --rm -it -v $(pwd):/app/output -v "$src_dir":"$src_dir" alecmaly/sa-tool semgrep scan --exclude sg-rules --json --config auto --json-output=semgrep.json # --config ../sg-rules  # removing custom rules to increase speed
     src_dir=`pwd` && docker run --rm -it -v $(pwd):/app/output -v "$src_dir":"$src_dir" alecmaly/sa-tool python3 /app/semgrep-to-detector-results.py -b "$src_dir" 
 
-    # rust's was very large
-    rm grep-output.txt
 
     ## Grep to Detectors:
             # Example: adding if and loops to detectors
-    grep -rnEI --exclude-dir={.vscode,.git,node_modules,.json} "\bif\b" . | awk -F: '{print $1 ":" $2 ":" index($0, $4) ":" substr($0, index($0, $3))}' > grep-output.txt
+    grep -rnEI --exclude-dir={.vscode,.git,node_modules,.json,target} "\bif\b" . | awk -F: '{print $1 ":" $2 ":" index($0, $4) ":" substr($0, index($0, $3))}' > grep-output.txt
     src_dir=`pwd` && docker run --rm -it -v $(pwd):/app/output -v "$src_dir":"$src_dir" alecmaly/sa-tool python3 /app/grep-to-detector-results.py -b "$src_dir" -c "grep-if statements" -a
 
-    grep -rnEI --exclude-dir={.vscode,.git,node_modules,.json} "\b(while|for|until|do)\b" . | awk -F: '{print $1 ":" $2 ":" index($0, $4) ":" substr($0, index($0, $3))}' > grep-output.txt
+    grep -rnEI --exclude-dir={.vscode,.git,node_modules,.json,target} "\b(while|for|until|do)\b" . | awk -F: '{print $1 ":" $2 ":" index($0, $4) ":" substr($0, index($0, $3))}' > grep-output.txt
     src_dir=`pwd` && docker run --rm -it -v $(pwd):/app/output -v "$src_dir":"$src_dir" alecmaly/sa-tool python3 /app/grep-to-detector-results.py -b "$src_dir" -c "grep-loops" -a
 
-    # code .
+    # rust's was very large
+    rm grep-output.txt
+
+    code .
 done
 
+cd $BASE_DIR
 # docker image currently runs as root, may need to change ownership of files to user or possibly set `chmod 777 {}` if vs code extensions do not have permission to read files
 # sudo find . -exec chown $USER:$USER {} \;
